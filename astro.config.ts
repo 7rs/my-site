e1cbf984618da36bf8f5abd958c594c4c809e743
Cbrnex <31788262+7rs@users.noreply.github.com>
2024-09-23 20:17:57 +0900

    probably finished setup

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,25 @@
 import path from 'node:path';
 import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro';
 import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
+import UnoCSS from 'unocss/astro';
+import purgecss from 'astro-purgecss';
+import playformCompress from '@playform/compress';
 
 import tsconfig from './tsconfig.json';
 
+/**
+ * Registers the paths registered in compilerOptions.paths in tsconfig.json as alias for Vite.
+ *
+ * Example: 
+ * ```ts
+  {
+    "@layouts/*": ["src/layouts/*"],
+    "@components/*": ["src/components/*"]
+  }
+  ```
+ */
 function getAliasesArray() {
   let aliasesArray: { [key: string]: string } = {};
 
@@ -21,11 +36,20 @@ function getAliasesArray() {
 }
 
 export default defineConfig({
+  site: 'https://7rs.dev',
   vite: {
     resolve: {
       alias: getAliasesArray(),
     },
   },
-  site: 'https://7rs.dev',
-  integrations: [svelte(), UnoCSS()],
+  markdown: {
+    gfm: false,
+    shikiConfig: {
+      theme: 'dracula',
+    },
+  },
+  build: {
+    assets: Math.random().toString(36).substring(2),
+  },
+  integrations: [svelte(), icon(), mdx(), UnoCSS(), purgecss(), playformCompress()],
 });
