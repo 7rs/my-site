d323f4207ba6bca71191fdae318287f5f5895a6e
Cbrnex <31788262+7rs@users.noreply.github.com>
2024-09-26 23:28:59 +0900

    quit support responsive manually

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,13 @@
 import { defineConfig } from 'unocss';
 
-export default defineConfig({});
+export default defineConfig({
+  theme: {
+    breakpoints: {
+      xs: '320px',
+      sm: '512px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+  },
+});
