a72ff107761e596321d1203306f92d617e2a37ce
Cbrnex <31788262+7rs@users.noreply.github.com>
2025-01-08 14:58:44 +0900

    feat: refactor and fix mobile layout

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,4 @@
-import { defineConfig } from 'unocss';
-import { presetUno } from 'unocss';
+import { defineConfig, presetUno } from 'unocss';
 
 const px = (scale) => `${320 * scale}px`;
 
@@ -13,9 +12,14 @@ export default defineConfig({
   shortcuts: {
     'icon-text': 'p-2 flex items-center gap-2 [&>svg]:w-4 [&>svg]:h-4 text-inherit',
     'icon-big': 'hover:scale-125 transition-all text-inherit',
+    'rounded-shadow': 'hover:bg-slate-400 rounded-xl transition-all duration-200',
+    // Footer
+    'footer-frame': 'py-6 px-8 flex justify-center items-center bg-sub',
+    'footer-body': 'w-full flex flex-col gap-4 text-main max-w-[var(--md-width)] md:max-w-[var(--lg-width)]',
+    'social-icons': 'flex flex-wrap gap-5 justify-center items-center',
+    'nav-list': 'flex flex-wrap justify-around',
     'nav-link': 'underline decoration-1.5 text-main font-bold hover:text-link hover:decoration-0 transition-all',
     'nav-active': 'nav-link text-active',
-    'rounded-shadow': 'hover:bg-slate-400 rounded-xl transition-all',
   },
   theme: {
     breakpoints: {
