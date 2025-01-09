0de48bdf39bea66d7165e9fdfa5bb54b7748800b
Cbrnex <31788262+7rs@users.noreply.github.com>
2025-01-09 21:46:17 +0900

    I tried oxc, but did not work.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,46 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintPluginAstro from "eslint-plugin-astro";
-import eslintPluginSvelte from "eslint-plugin-svelte";
-import jsxA11y from "eslint-plugin-jsx-a11y";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import pluginPromise from 'eslint-plugin-promise';
+import nodePlugin from 'eslint-plugin-n';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import vitest from '@vitest/eslint-plugin';
+// import oxlint from 'eslint-plugin-oxlint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
-  ...eslintPluginSvelte.configs["flat/recommended"],
+  importPlugin.flatConfigs.recommended,
+  pluginPromise.configs['flat/recommended'],
+  nodePlugin.configs['flat/recommended-script'],
   jsxA11y.flatConfigs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginSvelte.configs['flat/recommended'],
+  vitest.configs.recommended,
+  // oxlint.configs['flat/recommended'],
+
   {
-    ignores: ["**/.astro/**", "**/.vscode/**", "**/dist/**", "**/node_modules/**", "./docs/**"],
-  },
-  {
-    files: ["env.d.ts", "**/env.d.ts"],
+    ignores: ['**/.astro/**', '**/.vscode/**', '**/dist/**', '**/node_modules/**', './docs/**'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
     rules: {
-      "@typescript-eslint/triple-slash-reference": "off",
+      'import/no-named-as-default-member': 'off',
+      'import/no-unresolved': 'off',
+    },
+  },
+
+  {
+    files: ['env.d.ts', '**/env.d.ts'],
+    rules: {
+      '@typescript-eslint/triple-slash-reference': 'off',
     },
   },
 );
